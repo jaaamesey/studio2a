95b95522cb221a9923ca2f1849463ffe9afdf4df
Jamie Karlsson <jkarlsson@mcfakefakestudios.com>
2020-07-27 20:43:17 +1000

    Added initial database dockerfile

---
+++ database/init.sql
@@ -0,0 +1,12 @@
+-- SCHEMA --
+CREATE TABLE test_table
+(
+    column1 varchar,
+    column2 varchar,
+    column3 varchar
+);
+
+-- DATA -- 
+INSERT INTO test_table
+VALUES
+    ('test1', 'test2', 'test3');
