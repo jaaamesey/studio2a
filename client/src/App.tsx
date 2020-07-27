2acf577f63e7b331266db8ccaa71ba20fff54539
Jamie Karlsson <jkarlsson@mcfakefakestudios.com>
2020-07-28 02:03:43 +1000

    Set up CORS and communication between frontend and backend

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,22 @@
 import React from 'react';
+import axios from 'axios';
 import logo from './logo.svg';
 import './App.css';
 
 interface AppProps {}
 
 function App({}: AppProps) {
+  const [result, setResult] = React.useState('');
+  React.useEffect(() => {
+    axios
+      .get('127.0.0.1:9000/memes')
+      .then((res) => {
+        setResult('Connected to server successfully.');
+      })
+      .catch((err) => {
+        setResult(err.toString());
+      });
+  });
   return (
     <div className="App">
       <header className="App-header">
@@ -12,6 +24,7 @@ function App({}: AppProps) {
         <p>
           Edit <code>src/App.tsx</code> and save to reload. (plz)
         </p>
+        <p>Result from server: {result}</p>
         <a
           className="App-link"
           href="https://reactjs.org"
