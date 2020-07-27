793723135f544df8d0896f9ee3cb46fbafa68519
Jamie Karlsson <jkarlsson@mcfakefakestudios.com>
2020-07-28 00:14:31 +1000

    Server stuff

---
+++ server/src/app.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class AppService {
+  getMemes(): string {
+    return 'memes';
+  }
+  getHello(): string {
+    return 'Hello world!';
+  }
+}
