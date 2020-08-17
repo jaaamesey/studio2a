30f1ab6fce58fa070ca4d43b9c14062fa8c43df7
Jaffar Rizvi <J.Rizvi@outlook.com>
2020-08-17 10:40:24 +0000

    Aligns registration first name with rest of form

--- client/src/components/pages/registration.tsx
+++ client/src/components/pages/registration.tsx
@@ -6,13 +6,13 @@ export const RegistrationPage: React.FC = () => {
     <div className="form-div">
       <form action="" id="registration-form">
         <h2>Sign up</h2>
-        <div className="regist-name-divs">
+        <div className="firstname-div">
           <label htmlFor="first_name">First Name</label>
           <br />
           <input type="text" size={20} name="first_name" required />
         </div>
 
-        <div className="regist-name-divs">
+        <div className="lastname-div">
           <label htmlFor="last_name">Last Name</label>
           <br />
           <input type="text" size={20} name="last_name" required />
