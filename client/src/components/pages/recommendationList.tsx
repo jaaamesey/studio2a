341bb8784691277daa1ae08ffd939722e574dc80
Thouny <anthony.vu@hotmail.fr>
2020-08-26 14:09:19 +1000

    Recommendation list page

---
+++ client/src/components/pages/recommendationList.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../../res/css/forms.css';
+
+export const RecommendationList: React.FC = () => {
+  return (
+    <div>      
+      <h1>Course Recommendation</h1>
+    </div>
+  );
+};
