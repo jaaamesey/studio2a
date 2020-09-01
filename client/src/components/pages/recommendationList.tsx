e7ade83e53cb86de12261d70bbe5cf6e3deba558
Thouny <anthony.vu@hotmail.fr>
2020-09-01 18:48:25 +1000

    Add multi modal / CSS style adjustment

--- client/src/components/pages/recommendationList.tsx
+++ client/src/components/pages/recommendationList.tsx
@@ -1,39 +1,46 @@
 import React, { useState } from 'react';
-//import '../../res/css/style.css';
 import '../../res/css/tables.css';
 import { Button, Container, Col, Modal, Row } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
+import { faChevronLeft } from '@fortawesome/free-solid-svg-icons';
 
 export const RecommendationList: React.FC = () => {
-  const [show, setShow] = useState(false);
+  const [modalState, setModalState] = useState<
+    | 'modal-one'
+    | 'modal-two'
+    | 'modal-three'
+    | 'modal-four'
+    | 'modal-five'
+    | 'close'
+  >('close');
 
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
+  const handleShowModalOne = () => setModalState('modal-one');
+  const handleShowModalTwo = () => setModalState('modal-two');
+  const handleShowModalThree = () => setModalState('modal-three');
+  const handleShowModalFour = () => setModalState('modal-four');
+  const handleShowModalFive = () => setModalState('modal-five');
+
+  const handleClose = () => setModalState('close');
 
   return (
     <div>
-      <div className="back-button">
-        <Button className="btn">
-          {' '}
-          <FontAwesomeIcon icon={faArrowLeft} size="2x" />
-        </Button>
-      </div>
+      <button className="back-button">
+        {' '}
+        <FontAwesomeIcon icon={faChevronLeft} size="2x" />
+      </button>
 
       <Container>
         <Row>
-          <Col sm={2} className="non">
-            <h4>Tags choosen:</h4>
+          <Col sm={2} className="tag-title">
+            <h4>Tags chosen:</h4>
           </Col>
           <Col sm={9} className="tag-panel">
             <Row>
               <Button className="btn btn-primary tag-btn">
                 Problem Solving
               </Button>
-
               <Button className="btn btn-default tag-btn">Leadership</Button>
-
-              <Button className="btn btn-default tag-btn">Management</Button>
+              <Button className="btn btn-default tag-btn">Creativity</Button>
               <Button className="btn btn-default tag-btn">Management</Button>
             </Row>
           </Col>
@@ -48,7 +55,7 @@ export const RecommendationList: React.FC = () => {
 
       <table className="content-table">
         <thead>
-          <tr onClick={handleShow}>
+          <tr>
             <th>#</th>
             <th>Course Code</th>
             <th>Title</th>
@@ -58,7 +65,7 @@ export const RecommendationList: React.FC = () => {
           </tr>
         </thead>
         <tbody>
-          <tr>
+          <tr onClick={handleShowModalOne}>
             <td>1</td>
             <td>C12345</td>
             <td>Bachelor of Engineering (Honours)</td>
@@ -66,7 +73,7 @@ export const RecommendationList: React.FC = () => {
             <td>Undergraduate</td>
             <td>4 years</td>
           </tr>
-          <tr>
+          <tr onClick={handleShowModalTwo}>
             <td>2</td>
             <td>C54321</td>
             <td>Bachelor of Commerce</td>
@@ -74,7 +81,7 @@ export const RecommendationList: React.FC = () => {
             <td>Undergraduate</td>
             <td>3 years</td>
           </tr>
-          <tr>
+          <tr onClick={handleShowModalThree}>
             <td>3</td>
             <td>C23564</td>
             <td>Master of Law</td>
@@ -82,7 +89,7 @@ export const RecommendationList: React.FC = () => {
             <td>Postgraduate</td>
             <td>2 years</td>
           </tr>
-          <tr>
+          <tr onClick={handleShowModalFour}>
             <td>4</td>
             <td>C23165</td>
             <td>Bachelor of Information Technology</td>
@@ -90,7 +97,7 @@ export const RecommendationList: React.FC = () => {
             <td>Undergraduate</td>
             <td>3 years</td>
           </tr>
-          <tr>
+          <tr onClick={handleShowModalFive}>
             <td>5</td>
             <td>C25365</td>
             <td>Bachelor of Science</td>
@@ -101,24 +108,113 @@ export const RecommendationList: React.FC = () => {
         </tbody>
       </table>
 
-      <div>
-        <Button
-          variant="primary"
-          className="btn btn-primary"
-          onClick={handleShow}
-        >
-          Launch demo modal
-        </Button>
-      </div>
-
       <Container>
-        <Modal show={show} onHide={handleClose}>
+        <Modal show={modalState === 'modal-one'} onHide={handleClose}>
           <Modal.Header closeButton>
-            <Modal.Title>Modal heading</Modal.Title>
+            <Modal.Title>Bachelor of Engineering (Honours)</Modal.Title>
           </Modal.Header>
-          <Modal.Body>Whoooo readung this text in a modal !</Modal.Body>
+          <Modal.Body>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur.
+          </Modal.Body>
           <Modal.Footer>
-            <Button variant="secondary" onClick={handleClose}>
+            <Button
+              variant="secondary"
+              className="btn btn-default"
+              onClick={handleClose}
+            >
+              Close
+            </Button>
+          </Modal.Footer>
+        </Modal>
+        <Modal show={modalState === 'modal-two'} onHide={handleClose}>
+          <Modal.Header closeButton>
+            <Modal.Title>Bachelor of Commerce</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur.
+          </Modal.Body>
+          <Modal.Footer>
+            <Button
+              variant="secondary"
+              className="btn btn-default"
+              onClick={handleClose}
+            >
+              Close
+            </Button>
+          </Modal.Footer>
+        </Modal>
+        <Modal show={modalState === 'modal-three'} onHide={handleClose}>
+          <Modal.Header closeButton>
+            <Modal.Title>Master of Law</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur.
+          </Modal.Body>
+          <Modal.Footer>
+            <Button
+              variant="secondary"
+              className="btn btn-default"
+              onClick={handleClose}
+            >
+              Close
+            </Button>
+          </Modal.Footer>
+        </Modal>
+        <Modal show={modalState === 'modal-four'} onHide={handleClose}>
+          <Modal.Header closeButton>
+            <Modal.Title>Bachelor of Information Technology</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur.
+          </Modal.Body>
+          <Modal.Footer>
+            <Button
+              variant="secondary"
+              className="btn btn-default"
+              onClick={handleClose}
+            >
+              Close
+            </Button>
+          </Modal.Footer>
+        </Modal>
+        <Modal show={modalState === 'modal-five'} onHide={handleClose}>
+          <Modal.Header closeButton>
+            <Modal.Title>Bachelor of Science</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur.
+          </Modal.Body>
+          <Modal.Footer>
+            <Button
+              variant="secondary"
+              className="btn btn-default"
+              onClick={handleClose}
+            >
               Close
             </Button>
           </Modal.Footer>
