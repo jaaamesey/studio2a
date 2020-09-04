68345050caca97005c2d499b1751c4e054d60f6e
Thouny <anthony.vu@hotmail.fr>
2020-09-04 16:56:54 +1000

    add accurate course info (mock data) + link to actual UTS handbook

--- client/src/components/pages/recommendationList.tsx
+++ client/src/components/pages/recommendationList.tsx
@@ -42,6 +42,12 @@ export const RecommendationList: React.FC = () => {
               <Button className="btn btn-default tag-btn">Leadership</Button>
               <Button className="btn btn-default tag-btn">Creativity</Button>
               <Button className="btn btn-default tag-btn">Management</Button>
+              <Button className="btn btn-default tag-btn">Management</Button>
+              <Button className="btn btn-default tag-btn">Management</Button>
+              <Button className="btn btn-default tag-btn">Management</Button>
+              <Button className="btn btn-default tag-btn">Management</Button>
+              <Button className="btn btn-default tag-btn">Management</Button>
+              <Button className="btn btn-default tag-btn">Management</Button>
             </Row>
           </Col>
         </Row>
@@ -67,7 +73,7 @@ export const RecommendationList: React.FC = () => {
         <tbody>
           <tr onClick={handleShowModalOne}>
             <td>1</td>
-            <td>C12345</td>
+            <td>C09066</td>
             <td>Bachelor of Engineering (Honours)</td>
             <td>Engineering and IT</td>
             <td>Undergraduate</td>
@@ -75,23 +81,23 @@ export const RecommendationList: React.FC = () => {
           </tr>
           <tr onClick={handleShowModalTwo}>
             <td>2</td>
-            <td>C54321</td>
-            <td>Bachelor of Commerce</td>
-            <td>Business School</td>
+            <td>C10026</td>
+            <td>Bachelor of Business</td>
+            <td>Business</td>
             <td>Undergraduate</td>
             <td>3 years</td>
           </tr>
           <tr onClick={handleShowModalThree}>
             <td>3</td>
-            <td>C23564</td>
-            <td>Master of Law</td>
+            <td>C04143</td>
+            <td>Master of Laws</td>
             <td>Law</td>
             <td>Postgraduate</td>
             <td>2 years</td>
           </tr>
           <tr onClick={handleShowModalFour}>
             <td>4</td>
-            <td>C23165</td>
+            <td>C10143</td>
             <td>Bachelor of Information Technology</td>
             <td>Engineering and IT</td>
             <td>Undergraduate</td>
@@ -99,7 +105,7 @@ export const RecommendationList: React.FC = () => {
           </tr>
           <tr onClick={handleShowModalFive}>
             <td>5</td>
-            <td>C25365</td>
+            <td>C10242</td>
             <td>Bachelor of Science</td>
             <td>Science</td>
             <td>Undergraduate</td>
@@ -114,12 +120,39 @@ export const RecommendationList: React.FC = () => {
             <Modal.Title>Bachelor of Engineering (Honours)</Modal.Title>
           </Modal.Header>
           <Modal.Body>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur.
+            <h5>Overiew:</h5>
+            <p>
+              This program is a comprehensive preparation for careers in the
+              professional practice of engineering. Students learn to deal with
+              complex systems and manage large-scale projects using the most
+              appropriate emerging technologies.
+            </p>
+            <p>
+              The course offers an authentic, professionally focused and
+              practice-based education program with two sessions of internship
+              (normally paid) in a real workplace setting. A number of the areas
+              of study are available with explicit specialisations. For example,
+              Civil Engineering is available with specialisations in Structures
+              and Construction. Students can also focus on or broaden their
+              studies by completing electives. By appropriate choice of
+              electives, students can gain knowledge in a second engineering
+              discipline, obtain a sub-major in a different field or study
+              postgraduate-level subjects and apply for credit towards an
+              engineering master degree. The concept has been strongly endorsed
+              in wide-ranging industry consultations. Interaction between work
+              experience and academic curriculum is very strong, giving the
+              program a depth that no other full-time academic course can match.
+            </p>
+            <hr />
+            <div className="link-course">
+              <a
+                href="https://www.handbook.uts.edu.au/courses/c09067.html"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Learn more
+              </a>
+            </div>
           </Modal.Body>
           <Modal.Footer>
             <Button
@@ -136,12 +169,37 @@ export const RecommendationList: React.FC = () => {
             <Modal.Title>Bachelor of Commerce</Modal.Title>
           </Modal.Header>
           <Modal.Body>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur.
+            <h5>Overiew:</h5>
+            <p>
+              The Bachelor of Business offers students a sound background in all
+              areas of business through eight common core subjects undertaken in
+              first year, followed by in-depth knowledge in one or more chosen
+              areas of interest through the completion of a mix of majors,
+              extended majors, sub-majors and electives.
+            </p>
+            <p>
+              Students have the flexibility to select a course structure and
+              major(s) after first year. This means they have the opportunity to
+              use Year 1 to explore all areas of business and ultimately guide
+              their chosen specialisation in Year 2 and 3. Students choose from
+              10 majors and 34 sub-majors, as well as four extended majors,
+              giving them the flexibility to study a broad range of
+              specialisations; or do an in-depth study of a specialisation of
+              their choosing. In some degree configurations, students have the
+              choice of four electives. This is an opportunity to deepen
+              expertise with additional business subjects, or add breadth by
+              enrolling in subjects from other university faculties.
+            </p>
+            <hr />
+            <div className="link-course">
+              <a
+                href="https://handbook.uts.edu.au/courses/c10026.html"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Learn more
+              </a>
+            </div>
           </Modal.Body>
           <Modal.Footer>
             <Button
@@ -158,12 +216,36 @@ export const RecommendationList: React.FC = () => {
             <Modal.Title>Master of Law</Modal.Title>
           </Modal.Header>
           <Modal.Body>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur.
+            <h5>Overiew:</h5>
+            <p>
+              This course gives students the opportunity to build specialist
+              legal skills in their area of professional practice, as well as
+              theoretical and practical research capabilities as they relate to
+              the study of the law.
+            </p>
+            <p>
+              The degree is highly customisable – students pursue 48 credit
+              points of study based on their personal or professional interests,
+              choosing from a wide range of law electives (such as finance,
+              patent, insurance, human rights, copyright, family, international
+              and common law) and majors (corporate and commercial law, dispute
+              resolution, and intellectual property). Course content is
+              delivered by a mix of practising law professionals, full-time
+              academic staff and international visiting academics, giving
+              students access to a wide range of expertise and perspectives on
+              the study and practice of law. Cross-institutional study in
+              Australia and overseas is encouraged as part of this degree.
+            </p>
+            <hr />
+            <div className="link-course">
+              <a
+                href="https://www.handbook.uts.edu.au/courses/c04143.html"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Learn more
+              </a>
+            </div>
           </Modal.Body>
           <Modal.Footer>
             <Button
@@ -180,12 +262,40 @@ export const RecommendationList: React.FC = () => {
             <Modal.Title>Bachelor of Information Technology</Modal.Title>
           </Modal.Header>
           <Modal.Body>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur.
+            <h5>Overiew:</h5>
+            <p>
+              This is a cooperative education scholarship program in computer
+              information systems, developed by UTS in cooperation with a group
+              of leading organisations. It differs from other cooperative
+              education courses in that, during the industry-based sessions,
+              students follow a structured program designed jointly by UTS and
+              the employer group, including formal coursework delivered by
+              industry.
+            </p>
+            <p>
+              There are a limited number of places available in this course
+              according to the number of industry sponsors each year. If
+              selected for this course, students receive a total scholarship of
+              around $49,500 for the duration of the course. This intensive
+              course includes two six-month industry-based sessions with
+              different industry sponsors. Industry sponsors are heavily
+              involved in the curriculum design to ensure graduates are not only
+              highly qualified but also have the knowledge and skills relevant
+              to the needs of industry. Students normally secure graduate
+              employment before completion of the course. Sponsors also actively
+              recruit graduates from the course but students are not obliged to
+              take up employment with a sponsor.
+            </p>
+            <hr />
+            <div className="link-course">
+              <a
+                href="https://www.handbook.uts.edu.au/courses/c10143.html"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Learn more
+              </a>
+            </div>
           </Modal.Body>
           <Modal.Footer>
             <Button
@@ -202,12 +312,39 @@ export const RecommendationList: React.FC = () => {
             <Modal.Title>Bachelor of Science</Modal.Title>
           </Modal.Header>
           <Modal.Body>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur.
+            <h5>Overiew:</h5>
+            <p>
+              The Bachelor of Science gives students a solid foundation in
+              scientific knowledge and practice while allowing them to
+              specialise in an area of interest. Students may follow any of the
+              nine different majors leading to the award of a degree naming the
+              chosen major, e.g. Bachelor of Science in Nanotechnology, or
+              Bachelor of Science in Medical Science, or any of the majors
+              available. Majors are chosen at the end of first year when
+              students have experienced a range of disciplines and are more
+              equipped to choose their preferred path. Students may also choose
+              not to follow a major, but to select a range of second- and
+              third-year subjects to tailor their study according to their
+              interests and graduate with a cross-disciplinary degree.
+            </p>
+            <p>
+              The flexibility of this course allows students to either
+              specialise in a specific professional area or to develop skills
+              and knowledge in a range of scientific disciplines. All majors aim
+              to produce professional scientists with a thorough grounding in
+              theory and highly adaptable and practical scientific, experimental
+              and computational skills relevant to the discipline chosen.
+            </p>
+            <hr />
+            <div className="link-course">
+              <a
+                href="https://www.handbook.uts.edu.au/courses/c10242.html"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Learn more
+              </a>
+            </div>
           </Modal.Body>
           <Modal.Footer>
             <Button
