53f0eda3a1b647bb9390d07dc8f9e73a9cd236c4
Jaffar Rizvi <J.Rizvi@outlook.com>
2020-09-05 01:54:06 +0000

    - Updates navbar with correct labels and styling - Links not set up

--- client/src/components/nav/navigationBar.tsx
+++ client/src/components/nav/navigationBar.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Navbar, Nav } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import '../../res/css/nav.css';
 
 export const NavigationBar: React.FC = () => {
   return (
@@ -8,18 +9,24 @@ export const NavigationBar: React.FC = () => {
       <Link to="/">
         <Navbar.Brand>Not UTS</Navbar.Brand>
       </Link>
-      <Nav className="mr-auto">
+      <Nav justify className="mr-auto">
         <Link to="/test" className="nav-link">
-          Test
+          Home
         </Link>
         <Link to="/test-2" className="nav-link">
-          Test 2
+          Recommend Courses
         </Link>
-        <Link to="/registration" className="nav-link">
-          Sign Up
+        <Link to="/test-2" className="nav-link">
+          View Courses
         </Link>
         <Link to="/login" className="nav-link">
-          Sign In
+          Login
+        </Link>
+        <Link to="/registration" className="nav-link">
+          Sign up
+        </Link>
+        <Link to="/test-2" className="nav-link">
+          Contact Us
         </Link>
       </Nav>
     </Navbar>
