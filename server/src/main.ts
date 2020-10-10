09ed62433991e9104ba9575aa3c32886df35c74c
David De Angelis <daviddeangelis@Davids-MacBook-Pro-2.local>
2020-10-10 15:40:32 +1100

    connect login screen to db

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,32 +1,19 @@
 import { NestFactory } from '@nestjs/core';
-import {
-  FastifyAdapter,
-  NestFastifyApplication,
-} from '@nestjs/platform-fastify';
-import pgPromise from 'pg-promise';
-
+import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
-
-const pgp = pgPromise();
-const db = pgp({
-  user: 'studio2a',
-  database: 'studio2a',
-  password: 'DEV_PASSWORD',
-  port: 5420,
-  host: 'host.docker.internal',
-});
+import { db } from './database';
 
 function testDatabase() {
   // "user" has to be wrapped in double quotes due to a conflict with postgres' own user table
   // See https://dba.stackexchange.com/questions/73136/cant-list-users-select-from-user-returns-only-one-row-with-one-column
-  db.any('SELECT us_name, us_email, us_type  FROM "user"').then((users) => {
+  db.any('SELECT us_name, us_email, us_type  FROM "user"').then((users: any) => {
     console.log(users);
   });
 
-  db.one('SELECT * FROM course WHERE co_id = 2').then((course) => {
+  db.one('SELECT * FROM course WHERE co_id = 2').then((course: any) => {
     console.log(course);
   });
-  db.oneOrNone('SELECT * FROM course WHERE co_id = 42069').then((course) => {
+  db.oneOrNone('SELECT * FROM course WHERE co_id = 42069').then((course: any) => {
     console.log(course);
   });
 }
@@ -38,7 +25,7 @@ async function bootstrap() {
     throw new Error(`Could not connect to database: ${e}`);
   }
 
-  testDatabase(); // Remove or comment this out once you're comfortable doing queries and stuff
+  //testDatabase(); // Remove or comment this out once you're comfortable doing queries and stuff
 
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
